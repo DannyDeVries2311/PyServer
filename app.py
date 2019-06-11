cf6d1edc04598f0def562ca7c51e88eda5383809
Danny de Vries <danny.de.vries@student.nhlstenden.com>
2019-06-11 12:48:41 +0200

    Code Changes

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, abort, jsonify
 import logging
+import psycopg2
+from config import config
+from flask import send_file
+import os
 
 
 app = Flask(__name__)
@@ -8,28 +12,72 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Server is running"
 
-@app.route("/temp", methods=['GET','POST'])
-def temp():
-    #""" Test to send value and receive value """
-    if request.method == 'POST':
-        average_time = request.form.get('average_time')
-        logging.warning("Test post")
-        #return "Hello World Post!"
-     
-    # request.method == 'GET'
-    return "Hello World Get!"
+@app.route("/addData", methods=['POST'])
+def addData():
+    data = request.get_json()
+    print(data)
+    text = data['data']
+    print(type(text))
+    print(text)
+    sql = "insert into testtable(data) values(%s) RETURNING id;"
 
-@app.route('/foo', methods=['POST']) 
-def foo():
-    if not request.json:
-        abort(400)
-    print(request.json)
-    #json_data = json.loads(request.json)
-    #json_string = json.dumps(request.json)
-    return json.dumps(request.json)
+    conn = None
+    dataID = None
 
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute(sql, (text,))
+        dataID = cur.fetchone()
+        conn.commit()
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return str(dataID)
+
+@app.route("/getAllData", methods=['GET'])
+def getAllData():
+    sql="""SELECT * FROM testtable"""
+    conn = None
+    data =   {}
+    dataList = []
+
+
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        cur.execute(sql)
+        allData = cur.fetchall()
+
+        for singleData in allData:
+
+            dataId = singleData[1]
+            text = singleData[0]
+
+            dataAsDict = {
+                'id:': dataId,
+                'text': text
+            }
+
+            dataList.append(dataAsDict);
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+    finally:
+        if conn is not None:
+            conn.close()
+        data.update({'data': dataList})
+        return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
